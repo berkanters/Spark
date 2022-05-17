06350dafe2d1b6aaa36e39bc2828c3e07a36e12e
Kaan Gürbüz <kaangurbuz97@gmail.com>
2022-05-17 15:08:16 +0300

    User DTO and mapping

--- Spark.API/Controllers/UserController.cs
+++ Spark.API/Controllers/UserController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Spark.API.DTOs;
 using Spark.Core.IntService;
 using Spark.Core.Models;
 
@@ -10,42 +12,43 @@ namespace Spark.API.Controllers
     public class UserController : ControllerBase
     {
         private IUserService _userService;
-
-        public UserController(IUserService userService)
+        private IMapper _mapper;
+        public UserController(IUserService userService, IMapper mapper)
         {
             _userService = userService;
+            _mapper = mapper;
         }
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
             var users = await _userService.GetAllAsync();
-            return Ok(users);
+            return Ok(_mapper.Map<IEnumerable<UserDto>>(users));
         }
 
         [HttpGet("{id:guid}")]
         public async Task<IActionResult> GetById(Guid id)
         {
             var user = await _userService.GetByIdAsync(id);
-            return Ok(user);
+            return Ok(_mapper.Map<UserDto>(user));
         }
 
-        [HttpGet("{gender:alpha}/{minAge:min}/{maxAge:min}")]
+        [HttpGet("{gender:alpha}/{minAge:int}/{maxAge:int}")]
         public async Task<IActionResult> GetByGenderAndAge(string gender, short minAge, short maxAge)
         {
             var user = await _userService.GetUserByGenderAndAge(gender, minAge, maxAge);
-            return Ok(user);
+            return Ok(_mapper.Map<UserDto>(user));
         }
 
         [HttpPost]
-        public async Task<IActionResult> Save(User user)
+        public async Task<IActionResult> Save(UserDto userDto)
         {
-            var users = await  _userService.AddAsync(user);
-            return Created(string.Empty, users);
+            var users = await _userService.AddAsync(_mapper.Map<User>(userDto));
+            return Created(string.Empty, userDto);
         }
         [HttpPut]
-        public  IActionResult Update(User user)
+        public  IActionResult Update(UserDto userDto)
         {
-            var users =  _userService.Update(user);
+            _userService.Update(_mapper.Map<User>(userDto));
             return NoContent();
         }
 
